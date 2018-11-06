2e43371da0a93ce7e33b2a277a2fd4a3ec4fc672
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-06 14:23:56 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,31 @@
-class Show < ActiveRecord::Base
+class Show < ActiveRecord::Base  
 
-  
-  
+  def self.highest_rating
+    Show.all.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.all.find_by(rating: self.highest_rating)
+  end
+
+  def self.lowest_rating
+    Show.all.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.all.find_by(rating: self.lowest_rating)
+  end
+
+  def self.ratings_sum
+    Show.all.sum(:rating)
+  end
+
+  def self.popular_shows
+    Show.all.where("rating > 5")
+  end
+
+  def self.shows_by_alphabetical_order
+    Show.all.order(:name)
+  end
+
 end
